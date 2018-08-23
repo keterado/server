3ac5e3d05c6a94c698b73202bf696f90dc11f0be
Keter <keter-1808@hotmail.com>
2018-08-22 21:46:31 -0500

    Initial commit

---
+++ controllers/users.controller.js
@@ -0,0 +1,84 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var userService = require('services/user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.put('/:_id', update);
+router.delete('/:_id', _delete);
+
+module.exports = router;
+
+function authenticate(req, res) {
+    userService.authenticate(req.body.correo, req.body.clave)
+        .then(function (admin) {
+            if (admin) {
+                // authentication successful
+                res.send(admin);
+            } else {
+                // authentication failed
+                res.status(400).send('Correo or password es incorrecta');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function register(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getAll(req, res) {
+    userService.getAll()
+        .then(function (admins) {
+            res.send(admins);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrent(req, res) {
+    userService.getById(req.admin.sub)
+        .then(function (admin) {
+            if (admin) {
+                res.send(admin);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function update(req, res) {
+    userService.update(req.params._id, req.body)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function _delete(req, res) {
+    userService.delete(req.params._id)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
---
+++ services/user.service.js
@@ -0,0 +1,201 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+var dbROOT = mongo.db(config.connectionString, { native_parser: true });
+db.bind('admins');
+dbROOT.bind('ROOT');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getAll = getAll;
+service.getById = getById;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+function authenticate(correo, clave) {
+    var deferred = Q.defer();
+
+    db.admins.findOne({ correo: correo }, function (err, admin) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (admin && bcrypt.compareSync(clave, admin.hash)) {
+            // authentication successful
+            deferred.resolve({
+                _id: admin._id,
+                correo: admin.correo,
+                nombres: admin.nombres,
+                apellidos: admin.apellidos,
+                cedula: admin.cedula,
+                token: jwt.sign({ sub: admin._id }, config.secret)
+            });
+        } else {
+            // authentication failed o verificar si es root
+            dbROOT.ROOT.findOne({correo: correo}, function (err, SuAdmin) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+// cambie aca, para q compare la clave del root sin hash
+                if (SuAdmin.clave == clave) {
+                    // authentication successful
+                    deferred.resolve({
+                        _id: SuAdmin._id,
+                        correo: SuAdmin.correo,
+                        nombres: SuAdmin.nombres,
+                        apellidos: SuAdmin.apellidos,
+                        cedula: SuAdmin.cedula,
+                        token: jwt.sign({ sub: SuAdmin._id }, config.secret)
+                    });
+                }else {
+                    // aqui falta los trabajadores si es que funca
+                    deferred.resolve();
+                }
+            });
+           // deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getAll() {
+    var deferred = Q.defer();
+
+    db.admins.find().toArray(function (err, admins) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        // return users (without hashed passwords)
+        admins = _.map(admins, function (admin) {
+            return _.omit(admin, 'hash');
+        });
+
+        deferred.resolve(admins);
+    });
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.admins.findById(_id, function (err, admin) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (admin) {
+            // return user (without hashed password)
+            deferred.resolve(_.omit(admin, 'hash'));
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function create(userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.admins.findOne(
+        { correo: userParam.correo },
+        function (err, admin) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (admin) {
+                // username already exists
+                deferred.reject('Correo "' + userParam.correo + '" is already taken');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        // set user object to userParam without the cleartext password
+        var admin = _.omit(userParam, 'clave');
+
+        // add hashed password to user object
+        admin.hash = bcrypt.hashSync(userParam.clave, 10);
+
+        db.admins.insert(
+            admin,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.admins.findById(_id, function (err, admin) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (admin.correo !== userParam.correo) {
+            // username has changed so check if the new username is already taken
+            db.admins.findOne(
+                { correo: userParam.correo },
+                function (err, admin) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (admin) {
+                        // username already exists
+                        deferred.reject('Correo "' + req.body.correo + '" is already taken')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+        // fields to update
+        var set = {
+            nombres: userParam.nombres,
+            apellidos: userParam.apellidos,
+            correo: userParam.correo
+        };
+
+        // update password if it was entered
+        if (userParam.clave) {
+            set.hash = bcrypt.hashSync(userParam.clave, 10);
+        }
+
+        db.admins.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.admins.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
